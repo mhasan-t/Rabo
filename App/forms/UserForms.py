67d300232887ff53bd82097d4706c4fd043682db
tahnoonn <tahnoonn19@gmail.com>
2021-05-05 00:26:51 +0600

    designed signup.html, wrote CreateUserForm

---
+++ App/forms/UserForms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+
+class CreateUserForm(forms.Form):
+    first_name = forms.CharField(max_length=20, widget=forms.TextInput(
+        attrs={'placeholder': 'enter your first name'}))
+    last_name = forms.CharField(max_length=20, widget=forms.TextInput(
+        attrs={'placeholder': 'enter your last name'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'placeholder': 'enter your e-mail'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'enter your password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'enter your password again'}))
+
+    def clean(self):
+        cleaned_data = super(CreateUserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
